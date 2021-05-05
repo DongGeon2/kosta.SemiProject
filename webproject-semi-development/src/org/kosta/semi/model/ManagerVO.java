d87dbb339264f68df34b132d64e8793adc60faec
Chenjae <injea95@naver.com>
2021-05-05 16:45:53 +0900

    ManagerVO 생성

---
+++ webproject-semi-development/src/org/kosta/semi/model/ManagerVO.java
@@ -0,0 +1,39 @@
+package org.kosta.semi.model;
+
+public class ManagerVO {
+	private String id;
+	private String password;
+	private String name;
+	public ManagerVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public ManagerVO(String id, String password, String name) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "ManagerVO [id=" + id + ", password=" + password + ", name=" + name + "]";
+	}
+}
