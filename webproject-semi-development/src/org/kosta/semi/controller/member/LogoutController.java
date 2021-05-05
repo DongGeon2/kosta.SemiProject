05177997bd3d84ee5e76b7104eab967a84451310
Chenjae <injea95@naver.com>
2021-05-05 17:43:41 +0900

    Logout controller 생성

---
+++ webproject-semi-development/src/org/kosta/semi/controller/member/LogoutController.java
@@ -0,0 +1,20 @@
+package org.kosta.semi.controller.member;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.kosta.semi.controller.common.Controller;
+
+public class LogoutController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		HttpSession session=request.getSession(false);
+		if(session!=null)
+			session.invalidate();
+		return "redirect:index.jsp";
+	}
+
+}
