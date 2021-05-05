f73dd0c8a3b3ff3a29c93e90018c8e735079da46
김정윤 <@DESKTOP-310LI0J>
2021-05-05 16:54:24 +0900

    PostVO 수정2

--- webproject-semi-development/src/org/kosta/semi/model/PostVO.java
+++ webproject-semi-development/src/org/kosta/semi/model/PostVO.java
@@ -9,4 +9,75 @@ public class PostVO {
 	private MemberVO memberVO;
 	private CountryVO countryVO;
 	private String catergory;
+	public PostVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public PostVO(String postNo, String postTitle, String postContent, int hits, String postTime, MemberVO memberVO,
+			CountryVO countryVO, String catergory) {
+		super();
+		this.postNo = postNo;
+		this.postTitle = postTitle;
+		this.postContent = postContent;
+		this.hits = hits;
+		this.postTime = postTime;
+		this.memberVO = memberVO;
+		this.countryVO = countryVO;
+		this.catergory = catergory;
+	}
+	public String getPostNo() {
+		return postNo;
+	}
+	public void setPostNo(String postNo) {
+		this.postNo = postNo;
+	}
+	public String getPostTitle() {
+		return postTitle;
+	}
+	public void setPostTitle(String postTitle) {
+		this.postTitle = postTitle;
+	}
+	public String getPostContent() {
+		return postContent;
+	}
+	public void setPostContent(String postContent) {
+		this.postContent = postContent;
+	}
+	public int getHits() {
+		return hits;
+	}
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+	public String getPostTime() {
+		return postTime;
+	}
+	public void setPostTime(String postTime) {
+		this.postTime = postTime;
+	}
+	public MemberVO getMemberVO() {
+		return memberVO;
+	}
+	public void setMemberVO(MemberVO memberVO) {
+		this.memberVO = memberVO;
+	}
+	public CountryVO getCountryVO() {
+		return countryVO;
+	}
+	public void setCountryVO(CountryVO countryVO) {
+		this.countryVO = countryVO;
+	}
+	public String getCatergory() {
+		return catergory;
+	}
+	public void setCatergory(String catergory) {
+		this.catergory = catergory;
+	}
+	@Override
+	public String toString() {
+		return "PostVO [postNo=" + postNo + ", postTitle=" + postTitle + ", postContent=" + postContent + ", hits="
+				+ hits + ", postTime=" + postTime + ", memberVO=" + memberVO + ", countryVO=" + countryVO
+				+ ", catergory=" + catergory + "]";
+	}
+	
 }
