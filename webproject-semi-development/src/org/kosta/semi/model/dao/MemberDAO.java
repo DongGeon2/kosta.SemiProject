d350d05b3e534d5743c3b59fbac447af9a4f8842
Chenjae <injea95@naver.com>
2021-05-05 17:30:39 +0900

    MemberDAO 추가 - 로그인, 조회

--- webproject-semi-development/src/org/kosta/semi/model/dao/MemberDAO.java
+++ webproject-semi-development/src/org/kosta/semi/model/dao/MemberDAO.java
@@ -7,7 +7,12 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.kosta.semi.model.vo.MemberVO;
+
 public class MemberDAO {
+	/**
+	 * MemberDAO에서는 회원정보관리에 관한 모든 메서드를 정의합니다
+	 */
 	private static MemberDAO dao=new MemberDAO();
 	private DataSource dataSource;
 	private MemberDAO(){
@@ -29,4 +34,62 @@ public class MemberDAO {
 		if(con!=null)
 			con.close();
 	}
+	/**
+	 * 회원정보로 로그인
+	 * @param id
+	 * @param password
+	 * @return MemberVO(id, password)
+	 * @throws SQLException
+	 */
+	public MemberVO login(String id,String password) throws SQLException{
+		MemberVO vo=null;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try{
+			con=dataSource.getConnection();
+			String sql="select name from board_member where id=? and password=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, password);
+			rs=pstmt.executeQuery();
+			if(rs.next()){
+				vo=new MemberVO();
+				vo.setId(id);
+				vo.setPassword(password);
+				vo.setName(rs.getString(1));
+			}
+		}finally{
+			closeAll(rs, pstmt,con);
+		}
+		return vo;
+	}
+	/**
+	 * id로 회원 이름 조회 
+	 * @param id
+	 * @return id,name
+	 * @throws SQLException
+	 */
+	public MemberVO findMemberById(String id) throws SQLException{
+		MemberVO vo=null;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try{
+			con=dataSource.getConnection();
+			String sql="select name from board_member where id=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs=pstmt.executeQuery();
+			if(rs.next()){
+				vo=new MemberVO();
+				vo.setId(id);
+				vo.setName(rs.getString(1));
+			}
+		}finally{
+			closeAll(rs, pstmt,con);
+		}
+		return vo;
+	}
+	
 }
