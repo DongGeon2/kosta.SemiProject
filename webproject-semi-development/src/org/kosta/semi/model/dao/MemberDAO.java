257af56d58e14a50ee9d7d7d8e24ae7448193a76
da02lee <ekdud822@gmail.com>
2021-05-05 18:51:08 +0900

    MemberDAO-findIdbyEmail,findPasswordByEmail

--- webproject-semi-development/src/org/kosta/semi/model/dao/MemberDAO.java
+++ webproject-semi-development/src/org/kosta/semi/model/dao/MemberDAO.java
@@ -9,6 +9,8 @@ import javax.sql.DataSource;
 
 import org.kosta.semi.model.vo.MemberVO;
 
+import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
+
 public class MemberDAO {
 	/**
 	 * MemberDAO에서는 회원정보관리에 관한 모든 메서드를 정의합니다
@@ -90,5 +92,58 @@ public class MemberDAO {
 		}
 		return vo;
 	}
-	
+	/**
+	 * email로 회원 id 조회
+	 * @param email
+	 * @return email,id
+	 * @throws SQLException
+	 */
+	public MemberVO findIdbyEmail(String email) throws SQLException{
+		MemberVO vo=null;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			con=dataSource.getConnection();
+			String sql="select id from board_member where email=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1,email);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				vo=new MemberVO();
+				vo.setEmail(email);
+				vo.setId(rs.getString(1));
+			}
+		}finally {
+			closeAll(rs, pstmt, con);
+		}
+		return vo;
+	}
+	/**
+	 * email로 회원 password 조회
+	 * @param email
+	 * @return email,password
+	 * @throws SQLException
+	 */
+	public MemberVO findPasswordByEmail(String email) throws SQLException{
+		MemberVO vo=null;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			con=dataSource.getConnection();
+			String sql="select password from board_member where email=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, email);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				vo=new MemberVO();
+				vo.setEmail(email);
+				vo.setPassword(rs.getString(1));
+			}
+		}finally {
+			closeAll(rs, pstmt, con);
+		}
+		return vo;
+	}
 }
