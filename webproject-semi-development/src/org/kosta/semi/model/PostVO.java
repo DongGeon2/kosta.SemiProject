026f3c993aaa1f3353d81a726b221da56385858d
김정윤 <@DESKTOP-310LI0J>
2021-05-05 16:51:40 +0900

    PostVO-수정

--- webproject-semi-development/src/org/kosta/semi/model/PostVO.java
+++ webproject-semi-development/src/org/kosta/semi/model/PostVO.java
@@ -1,11 +1,12 @@
 package org.kosta.semi.model;
 
 public class PostVO {
-	private int postNo;
+	private String postNo;
 	private String postTitle;
 	private String postContent;
 	private int hits;
 	private String postTime;
 	private MemberVO memberVO;
+	private CountryVO countryVO;
 	private String catergory;
 }
