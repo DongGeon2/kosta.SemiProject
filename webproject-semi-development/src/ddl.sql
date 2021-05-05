0e992cb932756bde6e8ccfcd5ed6f1db1d899dd2
Chenjae <injea95@naver.com>
2021-05-05 15:41:24 +0900

    SQL DDL 및 DML 업데이트

--- webproject-semi-development/src/ddl.sql
+++ webproject-semi-development/src/ddl.sql
@@ -1,5 +1,10 @@
---DDL
-
+DROP TABLE member; 
+DROP TABLE country;
+DROP TABLE manager;
+DROP TABLE member;
+DROP TABLE post;
+DROP TABLE postcomment;
+--1번
 CREATE TABLE country(
    country_id VARCHAR2(100) primary key,
    country_name VARCHAR2(100) not null,
@@ -7,7 +12,7 @@ CREATE TABLE country(
    language VARCHAR2(100) not null,
    currency VARCHAR2(100) not null
 )
-
+--4번
 CREATE TABLE post(
    post_no NUMBER primary key,
    country_id VARCHAR2(100) not null,
@@ -20,17 +25,17 @@ CREATE TABLE post(
    constraint fk_post_country foreign key(country_id) references country(country_id),
    constraint fk_post_member foreign key(member_id) references member(member_id)
 )
-
+--5번 
 CREATE TABLE postcomment( 
    comment_no NUMBER primary key,
    post_no NUMBER not null,
    member_id VARCHAR2(100) not null,
    content clob not null,
    time_commented DATE not null,
-   constraint fk_member_comment foreign key(member_id) references member(member_id),
-   constraint fk_post_comment foreign key(post_no) references post(post_no)
+   constraint fk_postcomment_member foreign key(member_id) references member(member_id),
+   constraint fk_postcomment_postforeign foreign key(post_no) references post(post_no)
 )
-
+--3번
 CREATE TABLE member(
    member_id VARCHAR2(100) primary key,
    password VARCHAR2(100) not null,
@@ -42,7 +47,7 @@ CREATE TABLE member(
    country_id VARCHAR2(100) not null,
    constraint fk_member_country foreign key(country_id) references country(country_id)
 )
-
+--2번
 CREATE TABLE manager(
    manager_id VARCHAR2(100) primary key,
    password VARCHAR2(100) not null,
