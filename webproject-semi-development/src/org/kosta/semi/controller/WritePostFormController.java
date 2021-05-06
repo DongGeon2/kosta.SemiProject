45e926c98904c47680e1609b3ff5fe50baa1491c
suisuiee <binnee7@hanmail.net>
2021-05-06 15:08:14 +0900

    git 충돌 제거

---
+++ webproject-semi-development/src/org/kosta/semi/controller/WritePostFormController.java
@@ -0,0 +1,19 @@
+package org.kosta.semi.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * WritePostFormController
+ * 글쓰기 BTN 클릭 -> 글쓰기 FORM으로 이동해주는 Controller
+ * @author SUE
+ *
+ */
+public class WritePostFormController implements Controller{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
