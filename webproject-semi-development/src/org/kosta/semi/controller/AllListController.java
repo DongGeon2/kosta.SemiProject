91d8f407ba87475752b5771a93a0f2013fcd6c00
suisuiee <binnee7@hanmail.net>
2021-05-06 19:04:35 +0900

    AllListController 충돌 수정

--- webproject-semi-development/src/org/kosta/semi/controller/AllListController.java
+++ webproject-semi-development/src/org/kosta/semi/controller/AllListController.java
@@ -9,11 +9,6 @@ import org.kosta.semi.model.PagingBean;
 import org.kosta.semi.model.PostDAO;
 import org.kosta.semi.model.PostVO;
 
-
-
-
-
-
 public class AllListController implements Controller {
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		int totalPostCount=PostDAO.getInstance().getTotalPostCount();
@@ -26,12 +21,11 @@ public class AllListController implements Controller {
 		}
 		
 		
-
 		request.setAttribute("pagingBean", pagingBean);
 		ArrayList<PostVO> list = PostDAO.getInstance().getAllPostingList(pagingBean);
 		request.setAttribute("list", list);
 		request.setAttribute("url", "/board/main-list.jsp");
-		
+		request.setAttribute("urlCountry", "/template/memberCount.jsp");
 		return "/template/layout.jsp";
 	}
 }
