2f4c998b533b95dc7b794750c3e88924a1cb0374
chenjae <injea95@naver.com>
2021-05-06 12:34:37 +0900

    login 수정 - 추가 수정 필요

--- webproject-semi-development/src/org/kosta/semi/controller/member/LoginController.java
+++ webproject-semi-development/src/org/kosta/semi/controller/member/LoginController.java
@@ -18,6 +18,7 @@ public class LoginController implements Controller {
 		if (mvo != null) {
 			HttpSession session = request.getSession();
 			session.setAttribute("mvo", mvo);	
+			System.out.println(mvo.toString());
 			return "redirect:index.jsp";
 		} else {			
 			return "redirect:member/login-fail.jsp";		
