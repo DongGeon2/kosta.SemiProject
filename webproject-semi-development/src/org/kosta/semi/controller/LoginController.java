c6d5d8fce032a50fc93db6f429c42e77bb6b31f6
suisuiee <binnee7@hanmail.net>
2021-05-07 14:36:35 +0900

    Login Controller 조회수 UPDATE 방지

--- webproject-semi-development/src/org/kosta/semi/controller/LoginController.java
+++ webproject-semi-development/src/org/kosta/semi/controller/LoginController.java
@@ -1,5 +1,7 @@
 package org.kosta.semi.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -16,7 +18,12 @@ public class LoginController implements Controller {
 		MemberVO mvo = MemberDAO.getInstance().login(id, password);
 		if (mvo != null) {
 			HttpSession session = request.getSession();
-			session.setAttribute("mvo", mvo);	
+			session.setAttribute("mvo", mvo);
+			/*
+		      읽은 게시물을 다시 읽었을 때 조회수 증가를 방지하기 위해 noList를 추가한다 
+			  PostDetailController(상세글보기 컨트롤러)에서 이용한다 
+			 */
+			session.setAttribute("noList",new ArrayList<String>());
 			return "redirect:index.jsp";
 		} else {			
 			return "redirect:member/login-fail.jsp";		
