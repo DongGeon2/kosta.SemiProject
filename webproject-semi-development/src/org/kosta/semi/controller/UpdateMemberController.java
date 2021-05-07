a52df59eab2a18ee9aa6c173ded1069d9ccea025
chenjae <injea95@naver.com>
2021-05-07 14:21:16 +0900

    회원정보 수정 관련

--- webproject-semi-development/src/org/kosta/semi/controller/UpdateMemberController.java
+++ webproject-semi-development/src/org/kosta/semi/controller/UpdateMemberController.java
@@ -2,13 +2,34 @@ package org.kosta.semi.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.kosta.semi.model.CountryDAO;
+import org.kosta.semi.model.CountryVO;
+import org.kosta.semi.model.MemberDAO;
+import org.kosta.semi.model.MemberVO;
 
 public class UpdateMemberController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		HttpSession session = request.getSession(false);
+		MemberVO mvo = (MemberVO)session.getAttribute("mvo");
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		String birth = request.getParameter("birth");
+		String travelstyle = request.getParameter("travelstyle");
+		String country = request.getParameter("country");
+		
+		mvo.setName(name);
+		mvo.setPassword(password);
+		mvo.setBirth(birth);
+		mvo.setTravelStyle(travelstyle);
+		CountryVO cvo = CountryDAO.getInstance().findCountryById(country);
+		mvo.setCountryVO(cvo);
+		
+		MemberDAO.getInstance().UpdateMember(mvo);
+		return "redirect:member/update-ok.jsp";	
 	}
 
 }
