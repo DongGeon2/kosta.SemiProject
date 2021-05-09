daa5cc4372248f32b3b57c1ab64d60e1979eab2f
suisuiee <binnee7@hanmail.net>
2021-05-09 16:00:14 +0900

    deletePostController

--- webproject-semi-development/src/org/kosta/semi/controller/DeletePostController.java
+++ webproject-semi-development/src/org/kosta/semi/controller/DeletePostController.java
@@ -4,12 +4,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.kosta.semi.model.PostDAO2;
+import org.kosta.semi.model.PostVO;
+
 public class DeletePostController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		HttpSession session = request.getSession(false);
-		
+		if(session == null || session.getAttribute("mvo") == null || request.getMethod().equals("POST") == false){
+			//return "redirect:AllListController.do";
+			
+		}
+		// pageNo -> postNo 수정해야함
+		String postNo = request.getParameter("pageNo");	
+		System.out.println(postNo);
+		//PostVO pvo = PostDAO2.getInstance().getPostingByNo(postNo);
+		PostDAO2.getInstance().deletePosting( Integer.parseInt(postNo));
+		//String countryName = pvo.getCountryVO().getCountryName();
 		return "redirect:AllListController.do";
 	}
 
