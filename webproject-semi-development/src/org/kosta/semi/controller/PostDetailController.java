11bd2ec478518eafba682543c0821c4ab0f22b30
suisuiee <binnee7@hanmail.net>
2021-05-11 15:11:44 +0900

    post-detail.....!

--- webproject-semi-development/src/org/kosta/semi/controller/PostDetailController.java
+++ webproject-semi-development/src/org/kosta/semi/controller/PostDetailController.java
@@ -64,9 +64,6 @@ public class PostDetailController implements Controller {
 		ArrayList<String> time = PostDAO.getInstance().getSysdateAndLocalTime(postNo);
 		request.setAttribute("time", time);
 		
-		//한국과 해당게시판의 나라별 시간
-		ArrayList<String> time = PostDAO.getInstance().getSysdateAndLocalTime(postNo);
-		request.setAttribute("time", time);
 		
 		request.setAttribute("pvo", pvo);
 		request.setAttribute("fvo", fvo);
