1ac8300361dbecf8c47a145fc4e178c5270ce100
chenjae <injea95@naver.com>
2021-05-12 14:39:30 +0900

    글작성 및 댓글작성 시 포인트 1점 부여

--- webproject-semi-development/src/org/kosta/semi/controller/WritePostController.java
+++ webproject-semi-development/src/org/kosta/semi/controller/WritePostController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.kosta.semi.model.CountryVO;
 import org.kosta.semi.model.FileDAO;
 import org.kosta.semi.model.FileVO;
+import org.kosta.semi.model.MemberDAO;
 import org.kosta.semi.model.MemberVO;
 import org.kosta.semi.model.PostDAO;
 import org.kosta.semi.model.PostVO;
@@ -66,7 +67,7 @@ public class WritePostController implements Controller {
 			PostDAO.getInstance().writeContent(pvo);//글쓰면 DB에 정보 들어가게하는 DAO
 			System.out.println(pvo.getPostNo());
 			System.out.println("글쓰기완료");
-			
+			MemberDAO.getInstance().addMemberPoint(mvo.getId(), 1, "글 작성");
 			//--------------------------------------------------------------------------------------------------------
 			
 			String filename = multi.getFilesystemName("filename");//저장된 파일명
