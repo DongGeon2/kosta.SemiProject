a4ffb063eff901c03dcf882bbf8f76ff45345ec4
chenjae <injea95@naver.com>
2021-05-12 09:53:59 +0900

    ddl member Point 관련 추가

--- webproject-semi-development/src/org/kosta/semi/sql/ddl.sql
+++ webproject-semi-development/src/org/kosta/semi/sql/ddl.sql
@@ -98,23 +98,6 @@ ALTER TABLE member MODIFY ( state NUMBER DEFAULT 1 ) ;
 -- !!삭제한 모든 회원 되살리기!!
 UPDATE member SET state=1 ;
 
--- member table 포인트 추가
-ALTER TABLE member ADD ( point NUMBER ) ;
-UPDATE member SET point=0 ;
-ALTER TABLE member MODIFY ( point NUMBER DEFAULT 0 ) ;
-
-
-create table member_timeline(
-	member_id varchar2(100) not null,	
-	acted_time date not null,
-	point number,
-	message clob not null,
-	primary key(member_id, acted_time)
-)
-
-INSERT INTO member_timeline (member_id, acted_time, point, message) VALUES(id, sysdate, point, message);
-
-
 
 
 ---------------------------------------------------likedb추가
@@ -138,6 +121,18 @@ WHERE post_no=42 order by time_commented DESC
 
 ALTER TABLE member MODIFY ( point NUMBER DEFAULT 0 );
 
+----------------------------------------------------------
+-- member table 포인트 추가
+ALTER TABLE member ADD ( point NUMBER ) ;
+UPDATE member SET point=0 ;
+ALTER TABLE member MODIFY ( point NUMBER DEFAULT 0 ) ;
 
 
-
+create table member_timeline(
+	member_id varchar2(100) not null,	
+	acted_time date not null,
+	point number,
+	message clob not null,
+	primary key(member_id, acted_time)
+)
+-----------------------------------------------------------------
